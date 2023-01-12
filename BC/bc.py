4821b5c5278f56eb9ae74bb2c7917ea0707e6c6f
martin-d-c <martindeclosets@yahoo.fr>
2023-01-12 15:28:24 +0000

    construct BC trainer

--- BC/bc.py
+++ BC/bc.py
@@ -1,31 +1,47 @@
 import gym
 from gym.spaces import Box, Dict
 from imitation.algorithms import bc
+from imitation.data.types import Trajectory
 import numpy as np
 # Define env and action spaces
+size_env = 3+4+1+3+4+3+4+3+3
+env_space = Box(low=-1., high=1.0, shape=(size_env,), dtype=np.float32)
+                    
 
-env_space = Dict(
-                {
-                    "tool_pos": Box(low=-1., high=1.0, shape=(3,), dtype=np.float32),
-                    "tool_quat": Box(low=-1., high=1.0, shape=(4,), dtype=np.float32),
-                    "tool_theta":Box(low=-1., high=1.0, shape=(1,), dtype=np.float32),
-                    'cube0_pos': Box(low=-1., high=1.0, shape=(3,), dtype=np.float32),
-                    'cube0_quat': Box(low=-1., high=1.0, shape=(4,), dtype=np.float32),
-                    'cube1_pos':Box(low=-1., high=1.0, shape=(3,), dtype=np.float32),
-                    'cube1_quat': Box(low=-1., high=1.0, shape=(4,), dtype=np.float32),
-                    'goal0_pos': Box(low=-1., high=1.0, shape=(3,), dtype=np.float32),
-                    'goal1_pos': Box(low=-1., high=1.0, shape=(3,), dtype=np.float32),
-                }
-            )
+action_space = Box(low=-1., high=1.0, shape=(3,), dtype=np.float32)
+ 
 
-action_space = Dict(
-    {
-        "xy_linear_velocity" : Box(low=-1., high=1.0, shape=(3,), dtype=np.float32)
-    }
+# Construct transition 
+transitions = []
 
-)
+folder = 'trajs/'
+n_episodes = 10
+for i in range(n_episodes):
+    filename = 'traj'+str(i)+'.npy'
+    traj = np.load(folder+filename, allow_pickle=True)
+    n_step,d = traj.shape
+    obs = []
+    acts = []
+    for x in traj:
+        
+        acts.append(x[1]['xy_linear_velocity'])
 
-transitions = np.load('transitions.npy', allow_pickle=True)
+        env = x[0]
+        field_env = ['tool_pos', 'tool_quat', 'tool_theta', 'cube0_pos', 'cube0_quat', 'cube1_pos', 'cube1_quat', 'goal0_pos', 'goal1_pos']
+        obs_=[]
+        for f in field_env:
+            if f == 'tool_theta':
+                obs_.append(env[f])
+                continue
+            for y in env[f]:
+                obs_.append(y)
+        obs.append(np.array(obs_))
+    obs.append(obs[-1])
+    transitions.append(Trajectory(obs=obs, acts=acts, infos=np.zeros(len(acts)), terminal=True))
+
+    
+
+
 rng = np.random.default_rng()
 
 bc_trainer = bc.BC(
--- BC/extract_data.py
+++ BC/extract_data.py
@@ -1,20 +1,41 @@
 import os
+import os.path
 import pickle
 import numpy as np
 
-transitions = []
+n_episodes = 10
 folder = '../../minimuse/output_dataset/'
-for filename in os.listdir(folder):
-    #file_name = '00000000_00000.pkl'
-    if filename != 'stats.pkl':
-        with (open(folder+filename, "rb")) as openfile:
-            while True:
-                try:
-                    step = pickle.load(openfile)
-                    del step[0]['rgb_top_camera']
-                    
-                    transitions.append(step)
-                except EOFError:
-                    break
+for i in range(n_episodes):
+    k=0
+    traj = []
+    while True :
+        if i <10:
+            filename = '0000000' + str(i) 
+        elif i<100:
+            filename = '000000' + str(i) 
+        else:
+            filename = '00000' + str(i) 
 
-np.save('transitions',transitions)
+        if k<10:
+            filename += '_0000' + str(k) + '.pkl'
+        elif k<100:
+            filename += '_000' + str(k) + '.pkl'
+        else:
+            filename += '_00' + str(k) + '.pkl'
+        path = folder + filename
+        if os.path.isfile(path):
+            with (open(path, "rb")) as openfile:
+                while True:
+                    try:
+                        step = pickle.load(openfile)
+                        del step[0]['rgb_top_camera']
+                        
+                        traj.append(step)
+                    except EOFError:
+                        break
+            k+=1
+        else:
+            print(path)
+            np.save('trajs/traj'+str(i),traj)
+            break
+
